b4c0e727ea1f4d85f1474d224eff04c9415d6765
Your Name <you@example.com>
2022-03-26 21:09:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let x = document.firstElementChild.lastElementChild.firstElementChild;
+x.textContent ="welcome";
+
+const list = document.getElementById('lists');
+list.lastElementChild.textContent = "welcome";
+
